c9b398a592f7a1a709d9c6ad7248aa449215c1e2
XuZihan <xuzihan.boy@gmail.com>
2018-01-08 12:22:57 +0800

    PCA
    imshow 20 eigenvector

--- MachineLearningPCA.m
+++ MachineLearningPCA.m
@@ -1,3 +1,21 @@
-load hald
-% filePath = './yale/s'+num2str(5)+'.bmp';
-img = imread('C:\Users\southwestCat\Documents\MATLAB\yale\s1.bmp');
+clear,clc
+imgSize = 100;
+imgNum = 165;
+filePath = '.\yale\s';
+feature = zeros(imgSize*imgSize,imgNum);
+for i = 1:imgNum
+    Path = strcat(filePath,num2str(i),".bmp");
+    img = imread(char(Path));
+%     img = img';
+    feature(:,i) = double(img(:));
+end
+% mean_feature = mean(feature,2);
+% mean_face = uint8(reshape(mean_feature,imgSize,imgSize));
+% imshow(mean_face);
+[pc,score,latent,tsquare] = pca(feature);
+feature_after_PCA = uint8(score(:,1:20));
+for i =1:20
+    figure(i)
+    face = uint8(reshape(feature_after_PCA(:,i),imgSize,imgSize));
+    imshow(face)
+end
